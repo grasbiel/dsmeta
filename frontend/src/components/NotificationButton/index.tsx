5ea41838f96fb3818a38803a7deb47fb2394f03a
grasbiel <bzsilva1998@gmail.com>
2022-09-08 23:02:43 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification.svg'
+import './style.css'
+function NotificationButton() {
+    return (
+
+        <div className='red-btn'>
+            <img src={icon} alt="Notificar"/> 
+        </div>
+
+    )
+}
+
+export default NotificationButton
