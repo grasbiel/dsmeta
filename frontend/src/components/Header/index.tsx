05d6623eaffe8d5b716c9844797123f9225d3032
grasbiel <bzsilva1998@gmail.com>
2022-09-10 13:25:42 -0300

    Other components

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,12 +1,16 @@
-import './'
+import logo from '../../assets/img/logo.svg'
+import './style.css'
 
-function Header () {
+function Header() {
     return (
-        <div className="logo-container">
-            <img src="images/logo.svg" alt="DSMeta" />
-            <h1>DSMeta</h1>
-            <p>Desenvolvido por <a href="">@grasbiel</a></p>
-        </div>
+        <header>
+            <div className="logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="">@grasbiel</a></p>
+            </div>
+        </header>
+
     )
 }
 
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,87 @@
+import NotificationButton from '../NotificationButton';
+import './style.css'
+
+
+function SalesCard() {
+    return (
+        <>
+            <div className="dsmeta-card">
+                <h2 className="dsmeta-title">Vendas</h2>
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+
+                <div className="dsmeta-form-control-container">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+
+                <div>
+                    <table className="dsmeta-sales-table">
+                        <thead className="">
+                            <tr>
+                                <th className="show992">ID</th>
+                                <th className="show576">Data</th>
+                                <th>Vendedor</th>
+                                <th className="show992">Visitas</th>
+                                <th className="show992">Vendas</th>
+                                <th>Total</th>
+                                <th>Notificar</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td className="show992">1</td>
+                                <td className="show576">08/07/2022</td>
+                                <td>Anakin</td>
+                                <td className="show992">100</td>
+                                <td className="show992">543</td>
+                                <td>R$ 2000,00</td>
+                                <td>
+                                    <div className="dsmeta-red-btn-container">
+                                        <div className="dsmeta-red-btn">
+                                            <NotificationButton />
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="show992">2</td>
+                                <td className="show576">09/07/2022</td>
+                                <td>Jonas</td>
+                                <td className="show992">300</td>
+                                <td className="show992">470</td>
+                                <td>R$ 3000,00</td>
+                                <td>
+                                    <div className="dsmeta-red-btn-container">
+                                        <div className="dsmeta-red-btn">
+                                            <NotificationButton />
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="show992">3</td>
+                                <td className="show576">11/09/2022</td>
+                                <td>Francisco</td>
+                                <td className="show992">200</td>
+                                <td className="show992">590</td>
+                                <td>R$ 5000,00</td>
+                                <td>
+                                    <div className="dsmeta-red-btn-container">
+                                        <div className="dsmeta-red-btn">
+                                            <NotificationButton />
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default SalesCard;
